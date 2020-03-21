7d3ae2f7c31e99212973b8c8aadd2d5f68526ecf
Stefan N <stefan@flitz.ch>
2020-03-21 23:48:21 +0100

    Added [wpb-random-posts] to functions.php

--- web/wp-content/themes/twentytwenty/functions.php
+++ web/wp-content/themes/twentytwenty/functions.php
@@ -43,6 +43,32 @@ function wpdocs_excerpt_more( $more ) {
 }
 add_filter( 'excerpt_more', 'wpdocs_excerpt_more' );
 
+function wpb_rand_posts() { 
+	$string = "";
+	$args = array(
+		'post_type' => 'post',
+		'orderby'   => 'rand',
+		'posts_per_page' => 5, 
+		);
+	$the_query = new WP_Query( $args );
+	if ( $the_query->have_posts() ) {
+		$string .= '<ul>';
+		while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$string .= '<li><a href="'. get_permalink() .'">'. get_the_title() .'</a></li>';
+		}
+		$string .= '</ul>';
+		/* Restore original Post Data */
+		wp_reset_postdata();
+	} else {
+		$string .= 'Keine Beiträge gefunden';
+	}
+	return $string; 
+	} 
+	 
+	add_shortcode('wpb-random-posts','wpb_rand_posts');
+	add_filter('widget_text', 'do_shortcode'); 
+
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
