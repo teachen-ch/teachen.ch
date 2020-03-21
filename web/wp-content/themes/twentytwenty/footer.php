5ab221696529297c88ab40c69883d2f33f385278
Stefan N <stefan@flitz.ch>
2020-03-21 16:33:50 +0100

    Added Matomo website stats tracking code

--- web/wp-content/themes/twentytwenty/footer.php
+++ web/wp-content/themes/twentytwenty/footer.php
@@ -22,4 +22,18 @@
 		<?php wp_footer(); ?>
 
 	</body>
+	<script>
+		var _paq = window._paq || [];
+  /* local analytics only. F$ck Google! */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.teachen.ch/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+		</script>
 </html>
+
