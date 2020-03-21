1bf5c5b975c5355da9689355c5db9cecd31ad515
Stefan N <stefan@flitz.ch>
2020-03-21 22:49:58 +0100

    updated askimet plugin

--- web/wp-content/themes/twentytwenty/footer.php
+++ web/wp-content/themes/twentytwenty/footer.php
@@ -23,17 +23,19 @@
 
 	</body>
 	<script>
-		var _paq = window._paq || [];
-  /* local analytics only. F$ck Google! */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//stats.teachen.ch/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-		</script>
+		if (document.location.hostname === 'teachen.ch') {
+			var _paq = window._paq || [];
+			/* local analytics only. F$ck Google! */
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u="//stats.teachen.ch/";
+				_paq.push(['setTrackerUrl', u+'matomo.php']);
+				_paq.push(['setSiteId', '1']);
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+			})();
+		}
+	</script>
 </html>
 
